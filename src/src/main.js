205e6e424a73ca7fb011c90166260426700fdf30
Alex <77962334+Alex-Gungair@users.noreply.github.com>
2022-08-22 19:57:52 +0800

    Add files via upload

---
+++ src/src/main.js
@@ -0,0 +1,9 @@
+import router from './router'
+import Vue from 'vue'
+import App from './App.vue'
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router:router
+}).$mount('#app')
